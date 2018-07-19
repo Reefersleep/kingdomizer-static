84ed03e3c286c5c58e4cc51f17df277fd93631dc
reefersleep <soerenknud@gmail.com>
2018-07-19 16:14:47 +0200

    Move :jvm-opts to :dev profile

--- project.clj
+++ project.clj
@@ -15,7 +15,6 @@
             [lein-figwheel "0.5.16"]]
 
   :min-lein-version "2.5.0"
-#_  :jvm-opts ["--add-modules" "java.xml.bind"]
   :clean-targets ^{:protect false}
   [:target-path
    [:cljsbuild :builds :app :compiler :output-dir]
@@ -63,6 +62,8 @@
                                   [org.clojure/tools.nrepl "0.2.13"]
                                   [com.cemerick/piggieback "0.2.2"]
                                   [pjstadig/humane-test-output "0.8.2"]]
+
+                   :jvm-opts ["--add-modules" "java.xml.bind"]
 
                    :plugins [[lein-figwheel "0.5.11"]
                              [lein-doo "0.1.8"]]}})
